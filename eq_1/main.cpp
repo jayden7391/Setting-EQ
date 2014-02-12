1e650dbaf2c49f7a10f16fcb4830b449de6da007
Jayden Sehee Lee <Jayden@Jaydens-MacBook-Air.local>
2014-02-12 13:56:34 +0900

    Initial Commit

---
+++ eq_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  eq_1
+//
+//  Created by Jayden Sehee Lee on 2/12/14.
+//  Copyright (c) 2014 Jayden Sehee Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
